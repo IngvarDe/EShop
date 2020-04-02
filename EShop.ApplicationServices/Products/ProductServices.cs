74c5c02f2850310909deeddcbd6b31c7c363e474
Ingvar Derešivski <inde999@gmail.com>
2020-04-02 16:56:46 +0300

    ProductCrud

--- EShop.ApplicationServices/Products/ProductServices.cs
+++ EShop.ApplicationServices/Products/ProductServices.cs
@@ -3,74 +3,75 @@ using EShop.Core.Dtos;
 using EShop.Core.ServiceInterface;
 using EShop.Core.ServiceResult;
 using EShop.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EShop.ApplicationServices.Products
 {
     public class ProductsService : IProductService
     {
-        private EShopDbContext _context { get; set; }
+        static EShopDbContext _context { get; set; }
 
         public ProductsService(EShopDbContext context)
         {
             _context = context;
         }
 
-        public ServiceResult<Product> Save(ProductDto dto)
+        public async Task<Product> GetAsync(Guid id)
+        {
+            var result = await _context.Product
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
+        }
+
+        public async Task<Product> Add(ProductDto dto)
         {
             Product product = new Product
             {
                 Id = Guid.NewGuid(),
                 Name = dto.Name,
                 Description = dto.Description,
-                Value = dto.Value
+                Value = dto.Value,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now
             };
 
-            _context.Product.Add(product);
-            _context.SaveChanges();
-            return ServiceResult<Product>.Ok(product);
+            await _context.Product.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
         }
 
-        public ServiceResult<Product> Edit(ProductDto dto)
+        public async Task<Product> Update(ProductDto dto)
         {
             Product product = new Product
             {
                 Id = dto.Id,
                 Name = dto.Name,
                 Description = dto.Description,
-                Value = dto.Value
+                Value = dto.Value,
+                CreatedAt = dto.CreatedAt,
+                ModifiedAt = DateTime.Now
             };
 
-            _context.Product.Add(product);
-            _context.SaveChanges();
-            return ServiceResult<Product>.Ok(product);
+            _context.Product.Update(product);
+            await _context.SaveChangesAsync();
+            return product;
         }
 
-        public ServiceResults Delete(ProductDto dto)
+        public async Task<Product> Delete(Guid id)
         {
-            var productId = _context.Product
-                .FirstOrDefault(x => x.Id == dto.Id);
+            var productId = await _context.Product
+                .FirstOrDefaultAsync(x => x.Id == id);
 
             _context.Product.Remove(productId);
+            await _context.SaveChangesAsync();
 
-            return ServiceResults.Ok();
+            return productId;
         }
-
-        //public ServiceResults ProductGrid()
-        //{
-        //    var result = _context.Product
-        //        .Select(x => new ProductDto
-        //        {
-        //            Id = x.Id,
-        //            Description = x.Description,
-        //            Name = x.Name,
-        //            Value = x.Value
-        //        });
-
-        //    return ServiceResults.Ok();
-        //}
     }
 }
--- EShop.Core/Domain/Product.cs
+++ EShop.Core/Domain/Product.cs
@@ -5,9 +5,11 @@ namespace EShop.Core.Domain
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Value { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
     }
 }
