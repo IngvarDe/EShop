54d267be879abb7185f7de9dc8d3a66005aa1f0d
Ingvar Derešivski <inde999@gmail.com>
2020-04-15 18:31:03 +0300

    ProductFile_InProgress

--- EShop.Admin/Models/Product/ProductViewModel.cs
+++ EShop.Admin/Models/Product/ProductViewModel.cs
@@ -16,33 +16,4 @@ namespace EShop.Admin.Models.Product
         public string ExistingFilePath { get; set; }
         //public IEnumerable<FileDescriptionShort> Resources { get; set; } = new List<FileDescriptionShort>();
     }
-
-    //public class FileDescriptionShort
-    //{
-    //    public Guid? Id { get; set; }
-    //    public bool IsNew { get; set; }
-    //    public IFormFile File { get; set; }
-    //    //public ResourceType ResourceType { get; set; }
-    //    public string ResourceUrl { get; set; }
-    //    public string Title { get; set; }
-    //    public string ExternalUrl { get; set; }
-    //}
-
-    //public class FileResult
-    //{
-    //    public List<string> FileNames { get; set; }
-    //    public string Description { get; set; }
-    //    public DateTime CreatedAt { get; set; }
-    //    public DateTime ModifiedAt { get; set; }
-    //    public List<string> ContentTypes { get; set; }
-    //}
-
-    //public class FileDescription
-    //{
-    //    public List<string> FileNames { get; set; }
-    //    public string Description { get; set; }
-    //    public DateTime CreatedAt { get; set; }
-    //    public DateTime ModifiedAt { get; set; }
-    //    public string ContentTypes { get; set; }
-    //}
 }
