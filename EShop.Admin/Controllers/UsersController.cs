106fdae5707ede4a03d64394dd4a14ec5dfe6c2c
Ingvar Derešivski <inde999@gmail.com>
2020-05-21 16:54:01 +0300

    UserViewAndRegister_InProgress

---
+++ EShop.Admin/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace EShop.Admin.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
