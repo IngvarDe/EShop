b4d4391545c9f84dcaddfef142ccbe2e4c79aedf
Ingvar Derešivski <inde999@gmail.com>
2020-07-14 16:59:20 +0300

    Commander_Complete

---
+++ EShop.Admin/Controllers/CommanderController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Admin.Models.Commander;
+using EShop.Core.Dtos;
+using EShop.Core.ServiceInterface;
+using EShop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EShop.Admin.Controllers
+{
+    public class CommanderController : Controller
+    {
+        private readonly ICommanderService _commander;
+        private readonly EShopDbContext _context;
+
+        public CommanderController(
+            ICommanderService commander,
+            EShopDbContext context
+            )
+        {
+            _commander = commander;
+            _context = context;
+        }
+
+        //Grid
+        public IActionResult Index()
+        {
+            var seed = _context.Command
+                .Select(x => new CommandGridListItem
+                {
+                    Id = x.Id,
+                    HowTo = x.HowTo,
+                    Line = x.Line,
+                    Platfrom = x.Platfrom
+                });
+
+            return View(seed);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var command = await _commander.GetAsyncId(id);
+
+            if (command == null)
+            {
+                return NotFound();
+            }
+
+            var model = new CommandViewModel()
+            {
+                Id = command.Id,
+                HowTo = command.HowTo,
+                Line = command.Line,
+                Platfrom = command.Platfrom
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CommandViewModel model)
+        {
+            var dto = new CommanderDto()
+            {
+                Id = model.Id,
+                HowTo = model.HowTo,
+                Line = model.Line,
+                Platfrom = model.Platfrom
+            };
+
+            var result = await _commander.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            CommandViewModel model = new CommandViewModel();
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CommandViewModel model)
+        {
+            var dto = new CommanderDto
+            {
+                Id = model.Id,
+                HowTo = model.HowTo,
+                Line = model.Line,
+                Platfrom = model.Platfrom
+            };
+
+            var result = await _commander.Add(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("index", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var commander = await _commander.Delete(id);
+
+            return RedirectToAction("index", commander);
+        }
+    }
+}
---
+++ EShop.Admin/Models/Commands/CommandViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace EShop.Admin.Models.Commander
+{
+    public class CommandViewModel
+    {
+        public Guid? Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platfrom { get; set; }
+    }
+}
