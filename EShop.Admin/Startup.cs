70cf482d168913fea01919278d094de8d976e03a
Ingvar Derešivski <inde999@gmail.com>
2020-07-14 17:46:30 +0300

    RestFixes

--- EShop.Admin/Startup.cs
+++ EShop.Admin/Startup.cs
@@ -37,7 +37,7 @@ namespace EShop.Admin
 
             services.AddScoped<IProductService, ProductsService>();
             services.AddScoped<ISpaceshipService, SapceshipServices>();
-            services.AddScoped<ICommanderService, CommanderServices>();
+            //services.AddScoped<ICommanderService, CommanderServices>();
 
             services.AddControllersWithViews();
         }
