2f957315c8e4562bb7c8af19fcd74528013e799e
Ingvar Derešivski <inde999@gmail.com>
2020-07-17 16:40:00 +0300

    WeatherDto_and_ServiceClass_InProgress

---
+++ EShop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,37 @@
+﻿using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using static EShop.Core.Dtos.WeatherForecastDto;
+
+namespace EShop.ApplicationServices.Services
+{
+    public class WeatherForecastServices
+    {
+        public string WeatherDetail(string City)
+        {
+            string appId = "8113fcc5a7494b0518bd91ef3acc074f";
+
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                RootObject weatherInfo = (new JavaScriptSerializer()).Deserialize<RootObject>(json);
+
+                ResultDto result = new ResultDto();
+
+                result.Country = weatherInfo.Sys.Country;
+                result.City = weatherInfo.Name;
+                //result Lat
+
+                return json;
+            }
+
+
+
+        }
+    }
+}
---
+++ EShop.Core/Dtos/WeatherForecastDto.cs
@@ -0,0 +1,87 @@
+﻿using Nancy.Diagnostics.Modules;
+using Nancy.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace EShop.Core.Dtos
+{
+    public class WeatherForecastDto
+    {
+        public class ResultDto
+        {
+            public string City { get; set; }
+            public string Country { get; set; }
+            public string Lat { get; set; }
+            public string Lon { get; set; }
+            public string Description { get; set; }
+            public string Humidity { get; set; }
+            public string TempFeelsLike { get; set; }
+            public string Temp { get; set; }
+            public string TempMax { get; set; }
+            public string TempMin { get; set; }
+            public string WeatherIcon { get; set; }
+        }
+
+        public class CoordDto
+        {
+            public double Lon { get; set; }
+            public double Lat { get; set; }
+        }
+
+        public class WeatherDto
+        {
+            public int Id { get; set; }
+            public string Main { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+        }
+
+        public class MainDto
+        {
+            public double Temp { get; set; }
+            public double Feels_Like { get; set; }
+            public int Temp_Min { get; set; }
+            public int Pressure { get; set; }
+            public int Humidity { get; set; }
+        }
+
+        public class WindDto
+        {
+            public double Speed { get; set; }
+            public int Deg { get; set; }
+        }
+
+        public class CloudsDto
+        {
+            public int All { get; set; }
+        }
+
+        public class SysDto
+        {
+            public int Type { get; set; }
+            public int Id { get; set; }
+            public string Country { get; set; }
+            public int Sunrise { get; set; }
+            public int Sunset { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Coord Coord { get; set; }
+            public List<Weather> Weather { get; set; }
+            public string @base { get; set; }
+            public Main Main { get; set; }
+            public int Visibility { get; set; }
+            public Wind Wind { get; set; }
+            public Clouds Clouds { get; set; }
+            public int Dt { get; set; }
+            public Sys Sys { get; set; }
+            public int TimeZone { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Cod { get; set; }
+        }
+    }
+}
